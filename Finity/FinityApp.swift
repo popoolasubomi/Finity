2b2d4509ad08028fcfa824d81c935dc44cb73f41
subomi <popoolaogooluwasubomi@gmail.com>
2022-11-16 23:11:52 -0500

    Initial Commit

---
+++ Finity/FinityApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinityApp.swift
+//  Finity
+//
+//  Created by Subomi Popoola on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct FinityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

25193b5ba7269dce1b80024b14e7f9923fecba77
subomi <popoolaogooluwasubomi@gmail.com>
2022-11-20 07:33:04 -0500

    Building UI

---
+++ Finity/Chats/ChatsView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatsView.swift
+//  Finity
+//
+//  Created by Subomi Popoola on 11/20/22.
+//
+
+import SwiftUI
+
+struct ChatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatsView()
+    }
+}
